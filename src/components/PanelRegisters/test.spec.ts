5c5150b21f305993b26705a0d27ede500250bfbb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-11-22 23:42:20 -0300

    test(unit): improvement mount PanelRegister with props

--- src/components/PanelRegisters/test.spec.ts
+++ src/components/PanelRegisters/test.spec.ts
@@ -1,9 +1,22 @@
 import { describe, expect, it } from 'vitest'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 
 import PanelRegisters from './index.vue'
 
-const wrapper = shallowMount(PanelRegisters)
+const wrapper = mount(PanelRegisters, {
+  props: {
+    type: 'investiment',
+    registers: [{
+      id: '1',
+      type: 'investiment',
+      date: '21/09/1994',
+      name: 'Test',
+      value: 2,
+      description: 'Test description of panel register',
+      pay: undefined,
+    }]
+  },
+})
 
 describe('Basic render PanelRegisters', () => {
   it('is PanelRegisters a vue instance', () => {
