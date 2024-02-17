6de47ff799367a4e9e197ff150ab75ad4a2e06b3
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-17 03:12:59 -0300

    test(unit): validation logo has svg

--- src/components/Logo/test.spec.ts
+++ src/components/Logo/test.spec.ts
@@ -9,4 +9,7 @@ describe('Basic render Logo', () => {
   it('is Logo a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
   })
+  it('in Logo has SVG', () => {
+    expect(wrapper.find('[aria-label="Logo Wallet Digitalz"]').exists()).toBeTruthy()
+  })
 })
