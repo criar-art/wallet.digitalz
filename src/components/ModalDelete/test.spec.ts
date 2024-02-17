abfc5a1e00ef77ba169b585f91c21f4ab71df693
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-17 03:50:07 -0300

    test(unit): improvement spec ModalDelete validation props

--- src/components/ModalDelete/test.spec.ts
+++ src/components/ModalDelete/test.spec.ts
@@ -3,22 +3,34 @@ import { shallowMount } from '@vue/test-utils'
 
 import ModalDelete from './index.vue'
 
+const mockRegister = {
+  id: 'test-1',
+  type: { label: 'Investiment', value: 'investiment' },
+  date: '1994-09-21',
+  name: 'Test',
+  value: 1000,
+  description: 'Description test example spec',
+  pay: false
+}
+
 const wrapper = shallowMount(ModalDelete, {
-  propsData: {
-    register: {
-      id: 'test-1',
-      type: { label: 'Investiment', value: 'investiment' },
-      date: '1994-09-21',
-      name: 'Test',
-      value: 1000,
-      description: 'Descriiption test example spec',
-      pay: false
-    }
-  }
+  propsData: { register: mockRegister }
 })
 
 describe('Basic render ModalDelete', () => {
   it('is ModalDelete a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+  it('Validation ModalDelete props', () => {
+    const propsRegister = wrapper.props().register
+    const { id, date, name, description, type, value, pay } = propsRegister
+    expect(propsRegister).toStrictEqual(mockRegister)
+    expect(id).toContain('test-1')
+    expect(type).toStrictEqual({ label: 'Investiment', value: 'investiment' })
+    expect(date).toContain('1994-09-21')
+    expect(name).toContain('Test')
+    expect(value).toBe(1000)
+    expect(description).toBe('Description test example spec')
+    expect(pay).toBeFalsy()
   })
 })
