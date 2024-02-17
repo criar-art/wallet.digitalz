411f1501cc26e7129d24b5c7407b0cc022ef20d6
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-17 04:14:46 -0300

    test(unit): improvement spec validation props PanelRegisters

--- src/components/PanelRegisters/test.spec.ts
+++ src/components/PanelRegisters/test.spec.ts
@@ -16,7 +16,18 @@ const wrapper = mount(PanelRegisters, {
       name: 'Test',
       value: 2,
       description: 'Test description of panel register',
-      pay: undefined,
+      pay: false,
+    }, {
+      id: '2',
+      type: {
+        label: 'Investiment',
+        value: 'investiment',
+      },
+      date: '21/09/1994',
+      name: 'Test',
+      value: 100,
+      description: 'Test description of panel register',
+      pay: true,
     }]
   },
 })
@@ -24,5 +35,27 @@ const wrapper = mount(PanelRegisters, {
 describe('Basic render PanelRegisters', () => {
   it('is PanelRegisters a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+  it('Validation PanelRegisters props', () => {
+    const { type, registers } = wrapper.props()
+
+    // First Investiment
+    expect(type).toContain('investiment')
+    expect(registers[0].id).toContain('1')
+    expect(registers[0].type).toStrictEqual({ label: 'Investiment', value: 'investiment' })
+    expect(registers[0].date).toContain('21/09/1994')
+    expect(registers[0].name).toContain('Test')
+    expect(registers[0].value).toBe(2)
+    expect(registers[0].description).toBe('Test description of panel register')
+    expect(registers[0].pay).toBeFalsy()
+
+    // Second Investment
+    expect(registers[1].id).toContain('2')
+    expect(registers[1].type).toStrictEqual({ label: 'Investiment', value: 'investiment' })
+    expect(registers[1].date).toContain('21/09/1994')
+    expect(registers[1].name).toContain('Test')
+    expect(registers[1].value).toBe(100)
+    expect(registers[1].description).toBe('Test description of panel register')
+    expect(registers[1].pay).toBeTruthy()
   })
 })
