b25958aca396ebd237fda9db927466aa329c7513
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-17 03:12:00 -0300

    test(unit): improvement spec test footer with content, logo and link

--- src/components/Footer/test.spec.ts
+++ src/components/Footer/test.spec.ts
@@ -1,12 +1,31 @@
 import { describe, expect, it } from 'vitest'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 
 import Footer from './index.vue'
-
-const wrapper = shallowMount(Footer)
+import pkg from '../../../package.json'
 
 describe('Basic render Footer', () => {
+  const appVersion: string = pkg.version
+  const wrapper = mount({
+    template: '<v-layout><Footer></Footer></v-layout>'
+  }, {
+    global: {
+      components: {
+        Footer,
+      }
+    }
+  })
+
   it('is Footer a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+  it('in Footer content text Year and Version', () => {
+    expect(wrapper.text()).toContain(`${ new Date().getFullYear() } — Versão ${appVersion}`)
+  })
+  it('in Footer has link for my Github', () => {
+    expect(wrapper.find('a').attributes().href).toContain('https://github.com/lucasferreiralimax')
+  })
+  it('in Footer has Logo', () => {
+    expect(wrapper.find('[aria-label="Logo Wallet Digitalz"]').exists()).toBeTruthy()
   })
 })
