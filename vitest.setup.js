ef0ca3259f577e69400b0847dc3353b02580576e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-17 03:11:26 -0300

    test(unit): adjustment config for vuetify and ResizeObserverMock

--- vitest.setup.js
+++ vitest.setup.js
@@ -3,10 +3,25 @@ import { vi } from 'vitest'
 
 import { createTestingPinia } from '@pinia/testing'
 import { createVuetify } from "vuetify";
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import i18n from '@/i18n'
+
+// Mock the ResizeObserver
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+// Stub the global ResizeObserver
+vi.stubGlobal('ResizeObserver', ResizeObserverMock);
 
 config.global.plugins = [
   createTestingPinia({ createSpy: vi.fn() }),
-  createVuetify(),
+  createVuetify({
+    components,
+    directives,
+  }),
   i18n()
 ]
