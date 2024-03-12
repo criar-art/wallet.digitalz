0bbbb5e04cd8108e159222522860c87c6a78a4e6
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-03-12 10:38:11 -0300

    test(unit): improvement test of  Information with classes and props check

--- src/components/Information/test.spec.ts
+++ src/components/Information/test.spec.ts
@@ -6,7 +6,7 @@ import Information from './index.vue'
 const wrapper = mount(Information, {
   props: {
     title: 'Test information',
-    type: 'investiment',
+    type: 'investment',
     value: '2',
   },
 })
@@ -14,5 +14,13 @@ const wrapper = mount(Information, {
 describe('Basic render Information', () => {
   it('is Information a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+  it('Check if the props are rendered correctly', () => {
+    expect(wrapper.find('.text-caption').text()).toBe('Test information');
+    expect(wrapper.find('b').text()).toBe('2');
+  })
+  it('Check if the class is applied correctly based on the type', () => {
+    expect(wrapper.classes()).toContain('highlight-top');
+    expect(wrapper.classes()).toContain('investment');
   })
 })
