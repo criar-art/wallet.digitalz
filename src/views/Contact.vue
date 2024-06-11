3bc6372645d65dd9469f8267d8733b16cad0b993
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-11 13:19:05 -0300

    update link contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,18 +1,30 @@
 <template>
-  <v-row justify="center" class="pa-sm-10 pa-4 pt-6 ma-0 background-world h-100">
+  <v-row
+    justify="center"
+    class="pa-sm-10 pa-4 pt-6 ma-0 background-world h-100"
+  >
     <v-col cols="auto">
       <v-card class="highlight-top pa-4 mb-4 elevation-2" max-width="450">
         <h2 class="mb-2 text-h5">
           <v-icon class="mr-2" icon="mdi-earth" />
-          {{ $t('contact.title') }}
+          {{ $t("contact.title") }}
         </h2>
         <p class="text-body-1 mb-2">
-          {{ $t('contact.text1') }}
+          {{ $t("contact.text1") }}
         </p>
         <p class="text-body-1 mt-2">
-          {{ $t('contact.text2') }}
+          {{ $t("contact.text2") }}
         </p>
         <ul class="ml-4">
+          <li class="text-body-1 mt-2">
+            <a
+              class="text-link"
+              href="https://criar.art/contact"
+              target="_blank"
+            >
+              criar.art
+            </a>
+          </li>
           <li class="text-body-1 mt-2">
             <a class="text-link" href="mailto:contato@criar.art">
               contato@criar.art
@@ -24,13 +36,12 @@
             </a>
           </li>
           <li class="text-body-1 mt-2">
-            <a class="text-link" href="https://lucas-frontend.web.app/contact" target="_blank">
-              lucas-frontend.web.app/contact
-            </a>
-          </li>
-          <li class="text-body-1 mt-2">
-            <a class="text-link" href="https://www.linkedin.com/in/lucasferreiralimax" target="_blank">
-              linkedin.com/in/lucasferreiralimax
+            <a
+              class="text-link"
+              href="https://www.instagram.com/criar.art.tecnologia"
+              target="_blank"
+            >
+              instagram.com/criar.art.tecnologia
             </a>
           </li>
         </ul>
@@ -41,7 +52,7 @@
 
 <style>
 .background-world {
-  background: url('@/assets/world.png') no-repeat;
+  background: url("@/assets/world.png") no-repeat;
   background-size: cover;
   background-position: center center;
 }
