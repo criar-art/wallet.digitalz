1a2d381e14e9432a54e48d5db3b756a440b2e811
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-12 02:40:48 -0300

    test(unit): update spec of button in contact

--- src/views/Contact.spec.ts
+++ src/views/Contact.spec.ts
@@ -5,8 +5,16 @@ import Contact from "./Contact.vue";
 
 const wrapper = mount(Contact);
 const card = wrapper.find(".highlight-top");
-const emailLinks = card.findAll("a.text-link");
-const externalLinks = card.findAll('a.text-link[target="_blank"');
+const emailCriarArt = wrapper.find(
+  '[aria-label="Email para contato@criar.art"]'
+);
+const emailLucas = wrapper.find(
+  '[aria-label="Email para lucasferreiralimax@gmail.com"]'
+);
+const linkCriarArt = wrapper.find('[aria-label="Acessar Criar.Art"]');
+const linkInstagram = wrapper.find(
+  '[aria-label="Acessar instagram criar.art.tecnologia"]'
+);
 
 describe("Basic render Contact", () => {
   it("is Contact a vue instance", () => {
@@ -18,22 +26,13 @@ describe("Basic render Contact", () => {
   it("Check if the card is rendered correctly", () => {
     expect(card.find("h2").text()).toBe("Contato");
     expect(card.findAll("p").length).toBe(2);
-    expect(card.findAll("li").length).toBe(4);
   });
   it("Check if the email links are rendered correctly", () => {
-    expect(emailLinks.length).toBe(4);
-    expect(emailLinks[1].attributes("href")).toBe("mailto:contato@criar.art");
-    expect(emailLinks[2].attributes("href")).toBe(
-      "mailto:lucasferreiralimax@gmail.com"
-    );
+    expect(emailCriarArt.text()).toEqual("contato@criar.art");
+    expect(emailLucas.text()).toEqual("lucasferreiralimax@gmail.com");
   });
   it("Check if the external links are rendered correctly", () => {
-    expect(externalLinks.length).toBe(2);
-    expect(externalLinks[0].attributes("href")).toBe("https://criar.art/contact");
-    expect(externalLinks[0].attributes("target")).toBe("_blank");
-    expect(externalLinks[1].attributes("href")).toBe(
-      "https://www.instagram.com/criar.art.tecnologia"
-    );
-    expect(externalLinks[1].attributes("target")).toBe("_blank");
+    expect(linkCriarArt.text()).toEqual("Criar.Art");
+    expect(linkInstagram.text()).toEqual("criar.art.tecnologia");
   });
 });
