a855f9a8111e47fa8bda962e59a7c380799fdbe0
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-25 12:48:46 -0300

    adjustment form modal registers

--- src/components/ModalRegister/index.vue
+++ src/components/ModalRegister/index.vue
@@ -15,7 +15,7 @@ const dialog = ref<boolean>(false)
 const valid = ref<boolean>(false)
 const myForm = ref()
 const typeRegister = ref<TypeRegister | undefined>(props.register ? props.register.type : undefined)
-const date = ref<string | undefined>(props.register ? props.register.date : new Date().toISOString().slice(0,10))
+const date = ref<string | undefined>(props.register ? props.register.date : new Date().toISOString().slice(0, 10))
 const name = ref<string | undefined>(props.register ? props.register.name : undefined)
 const value = ref<number | undefined>(props.register ? props.register.value : undefined)
 const description = ref<string | undefined>(props.register ? props.register.description : undefined)
@@ -39,15 +39,15 @@ const nameRules = [
 
 const valueRules = [
   (v: string) => !!v || t('register.form.rules.value.required'),
-  (v: number) => v <=  sizeLimitMoney || t('register.form.rules.value.less', { value: limitMoney })
+  (v: number) => v <= sizeLimitMoney || t('register.form.rules.value.less', { value: limitMoney })
 ]
 
-function close () {
+function close() {
   if (!props.update) myForm.value.reset()
   dialog.value = false
 }
 
-async function validate () {
+async function validate() {
   const { valid } = await myForm.value.validate()
 
   if (valid) {
@@ -78,30 +78,14 @@ async function validate () {
 </script>
 
 <template>
-  <v-dialog
-    v-model="dialog"
-    persistent
-  >
+  <v-dialog v-model="dialog" persistent>
     <template v-slot:activator="{ props }">
-      <v-btn
-        v-if="!update"
-        color="success"
-        v-bind="props"
-        variant="flat"
-        class="mr-sm-10 mr-4 teste"
-        aria-label="New register"
-        :size="xs ? 'small' : undefined"
-      >
+      <v-btn v-if="!update" color="success" v-bind="props" variant="flat" class="mr-sm-10 mr-4 teste"
+        aria-label="New register" :size="xs ? 'small' : undefined">
         <v-icon class="mr-2" icon="mdi-plus" />
         {{ $t('actions.register') }}
       </v-btn>
-      <v-btn
-        v-if="update"
-        color="success"
-        v-bind="props"
-        size="x-small"
-        icon="mdi-pencil"
-      />
+      <v-btn v-if="update" color="success" v-bind="props" size="x-small" icon="mdi-pencil" />
     </template>
     <v-card class="mx-auto pa-4" max-width="600" width="100%">
       <v-card-title>
@@ -111,85 +95,39 @@ async function validate () {
         <v-container class="pa-0">
           <v-form v-model="valid" ref="myForm">
             <v-row>
-              <v-col
-                cols="12"
-                class="pa-0 pb-2"
-              >
-                <v-select
-                  aria-label="Select type register"
-                  :label="$t('register.form.type')"
-                  v-model="typeRegister"
-                  :rules="typeRules"
-                  item-title="label"
-                  item-value="value"
-                  return-object
-                  required
-                  :items="[
+              <v-col cols="12" sm="6" class="pa-0 pr-sm-2">
+                <v-text-field aria-label="Date of register" :label="$t('register.form.date')" type="date" v-model="date"
+                  :rules="dateRules" required />
+              </v-col>
+              <v-col cols="12" sm="6" class="pa-0 pl-sm-2">
+                <v-select aria-label="Select type register" :label="$t('register.form.type')" v-model="typeRegister"
+                  :rules="typeRules" item-title="label" item-value="value" return-object required :items="[
                     { label: $t('register.form.investiment'), value: 'investiment' },
                     { label: $t('register.form.expense'), value: 'expense' },
                     { label: $t('register.form.entry'), value: 'entry' },
                     { label: $t('register.form.truck'), value: 'truck' },
                     { label: $t('register.form.vehicle'), value: 'vehicle' },
                     { label: $t('register.form.motorcycle'), value: 'motorcycle' },
-                  ]"
-                />
+                  ]" />
               </v-col>
-              <v-col
-                cols="12"
-                class="pa-0 pb-2"
-              >
-                <v-text-field
-                  aria-label="Date of register"
-                  :label="$t('register.form.date')"
-                  type="date"
-                  v-model="date"
-                  :rules="dateRules"
-                  required
-                />
+            </v-row>
+            <v-row>
+              <v-col cols="12" sm="6" class="pa-0 pr-sm-2">
+                <v-text-field aria-label="Name of register" :label="$t('register.form.name')" v-model="name"
+                  :rules="nameRules" required />
               </v-col>
-              <v-col
-                cols="12"
-                class="pa-0 pb-2"
-              >
-                <v-text-field
-                  aria-label="Name of register"
-                  :label="$t('register.form.name')"
-                  v-model="name"
-                  :rules="nameRules"
-                  required
-                />
+              <v-col cols="12" sm="6" class="pa-0 pl-sm-2">
+                <v-text-field aria-label="Value of register" :label="$t('register.form.value')" v-model="value"
+                  prefix="$" type="number" :rules="valueRules" required />
               </v-col>
-              <v-col
-                cols="12"
-                class="pa-0 pb-2"
-              >
-                <v-text-field
-                  aria-label="Value of register"
-                  :label="$t('register.form.value')"
-                  v-model="value"
-                  prefix="$"
-                  type="number"
-                  :rules="valueRules"
-                  required
-                />
-              </v-col>
-              <v-col
-                cols="12"
-                class="pa-0 pb-2"
-              >
-                <v-textarea
-                  aria-label="Description of register"
-                  :label="$t('register.form.description')"
-                  v-model="description"
-                  :maxlength="250"
-                />
+            </v-row>
+            <v-row>
+              <v-col cols="12" class="pa-0 pb-2">
+                <v-textarea aria-label="Description of register" :label="$t('register.form.description')"
+                  v-model="description" :maxlength="250" />
               </v-col>
               <template v-if="typeRegister?.value == 'expense'">
-                <v-checkbox
-                  aria-label="Pay of register"
-                  :label="t('register.form.pay')"
-                  v-model="pay"
-                />
+                <v-checkbox aria-label="Pay of register" :label="t('register.form.pay')" v-model="pay" />
               </template>
             </v-row>
           </v-form>
@@ -198,20 +136,11 @@ async function validate () {
       </v-card-text>
       <v-card-actions>
         <v-spacer />
-        <v-btn
-          color="error"
-          variant="text"
-          @click="close"
-        >
+        <v-btn color="error" variant="text" @click="close">
           <v-icon class="mr-2" icon="mdi-cancel" />
           {{ $t('actions.cancel') }}
         </v-btn>
-        <v-btn
-          aria-label="Create register"
-          color="success"
-          variant="flat"
-          @click="validate"
-        >
+        <v-btn aria-label="Create register" color="success" variant="flat" @click="validate">
           <v-icon class="mr-2" icon="mdi-check" />
           {{ update ? $t('actions.save') : $t('actions.create') }}
         </v-btn>
